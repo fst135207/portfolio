9d7500d893009da84cef83544dbf41be400496bf
fst135207 <felix.stalder.bsu@gmail.com>
2024-12-23 09:11:40 +0100

    Added Location and Socials ot the Left Side of the Portfolio

--- app/pages/LeftSide.tsx
+++ app/pages/LeftSide.tsx
@@ -1,5 +1,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import selfie from "@/public/selfie.jpg"
+import Snowypin from "@/public/snowypin.svg"
+import { Github, Linkedin } from "lucide-react"
+import { SiLeetcode } from "react-icons/si"
 
 export function LeftSide() {
   return (
@@ -16,6 +19,23 @@ export function LeftSide() {
           <p className="text-sm sm:text-base sm:ml-5 mt-1">
             Im a Platform Engineer working currently at Identitas AG
           </p>
+          <div className="flex items-center justify-between sm:ml-5 mt-2">
+            <div className="flex items-center">
+              <Snowypin className="w-5 h-5" />
+              <span className="ml-1 text-sm text-zinc-200">Bern, Switzerland</span>
+            </div>
+            <div className="flex gap-2">
+              <a href="https://www.linkedin.com/in/felix-stalder-6b20472a6" target="_blank" rel="noopener noreferrer">
+                <Linkedin className="w-5 h-5 hover:text-blue-600 transition-colors" />
+              </a>
+              <a href="https://leetcode.com/u/fst135207" target="_blank" rel="noopener noreferrer">
+                <SiLeetcode className="w-5 h-5 hover:text-yellow-500 transition-colors" />
+              </a>
+              <a href="https://github.com/fst135207" target="_blank" rel="noopener noreferrer">
+                <Github className="w-5 h-5 hover:text-gray-600 transition-colors" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
